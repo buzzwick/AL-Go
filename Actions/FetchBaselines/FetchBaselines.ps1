f6fbed9a64628322cdef7e9f605a266635536eb6
mazhelez <mazhelez@microsoft.com>
2023-02-09 14:53:00 +0100

    .

--- Actions/FetchBaselines/FetchBaselines.ps1
+++ Actions/FetchBaselines/FetchBaselines.ps1
@@ -6,6 +6,8 @@ param (
     $BuildMode
 )
 
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version 2.0
 $baseFolder = $ENV:GITHUB_WORKSPACE
 
 $projectPath = Join-Path $baseFolder $Project
@@ -17,7 +19,7 @@ if(-not (Test-Path $baselineScript)) {
 
 try {
     . (Join-Path -Path $PSScriptRoot -ChildPath "..\AL-Go-Helper.ps1" -Resolve)
-    $BcContainerHelperPath = DownloadAndImportBcContainerHelper -baseFolder $baseFolder
+    $bcContainerHelperPath = DownloadAndImportBcContainerHelper -baseFolder $baseFolder
 
     Write-Host 'Executing custom script to fetch baselines'
     . $baselineScript -Project $project -BuildMode $buildMode
